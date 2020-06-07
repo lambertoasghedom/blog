aeb121b3f2c32f410f7f9a9c774bc0d891548754
lambertoasghedom <asghedom.lamberto@gmail.com>
2020-06-06 19:28:59 -0700

    added codeshot

--- pages/2020/semantic-html.js
+++ pages/2020/semantic-html.js
@@ -2,6 +2,7 @@ import Layout from '../../components/layout';
 import Post from '../../components/post';
 import { Avatar, AvatarBadge, Stack } from "@chakra-ui/core";    
 import avatar from '../../img/avatar.jpeg';
+import code from '../../img/code.png'
 
 const SemanticHTML = () => (
    <Post>
@@ -20,9 +21,19 @@ const SemanticHTML = () => (
                We can verify that the semantics we're using are working in assistive technology becasue that's where the benefits come in. If you can navigate all the headings as a blind user, 
                that will save them a ton of time. If you can swap out a div for a more semantic element, I would recommened doing so. 
             </p>
-            <p>Defining styles with CSS is how we convey the sturcture of  a web page to humans. By marking it up with semantics, we can represent those visual styles to machines as well.</p>
+            <img src={code} alt="semantic html code block" />
+            <p>Defining styles with CSS is how we convey the sturcture of a web page to humans. By marking it up with semantics, we can represent those visual styles to machines as well.</p>
          </article>
       </main>
+      <style jsx>{`
+         img {
+            display: block;
+            width: 50%;
+            text-align: center;
+            margin-left: auto;
+            margin-right: auto;
+         }
+      `}</style>
    </Post>
 )
 
